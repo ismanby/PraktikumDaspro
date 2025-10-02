b49e95cbd650494c80fb6d6783a921399e9445b5
ismanby <ismaanby@gmail.com>
2025-10-02 10:07:31 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS12.java
+++ jobsheet5/ifCetakKRS12.java
@@ -8,12 +8,21 @@ public class ifCetakKRS12 {
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();    
 
+        // IF-ELSE
         if (uktLunas) {
+            System.out.println("-- Versi IF-ELSE --");
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
         } else {
             System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
+
+        // Ternary Operator
+        System.out.println("\n-- Versi Ternary Operator --");
+        String pesan = uktLunas ? "Pembayaran UKT terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA"
+                                : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+
+        System.out.println(pesan);
 
         sc.close();
     }
